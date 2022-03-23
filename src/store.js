5e7970592fe096c3d1056d9a398d19f17dc7dddd
Daniel <ugahdaniel1997@gmail.com>
2022-03-23 19:22:38 +0100

    hands off

--- src/store.js
+++ src/store.js
@@ -4,6 +4,25 @@ import reducers from './reducers';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 
+/*import { createStore, applyMiddleWare, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import {
+  hours,
+  weeks,
+  noOfStudents,
+  priceFactor,
+  priceOptions,
+  subject,
+  pricingDeterminant,
+  processingFee,
+  referral,
+} from './reducers'*/
+
+
+
 export function configureStore() {
   const persistentState = window.__REDUX_STATE__;
   // loadState();
@@ -12,6 +31,7 @@ export function configureStore() {
   if (process.env.NODE_ENV === 'production') {
     composeEnhancers = compose;
   }
+  
   const store = createStore(
     reducers,
     persistentState,
@@ -21,12 +41,24 @@ export function configureStore() {
 
   );
   sagaMiddleware.run(rootSaga);
-  // store.subscribe(throttle(() => {
-  //   saveState(
-  //     store.getState()
-  //   );
-  // }, 1000));
+   //store.subscribe(throttle(() => {
+     //saveState(
+       //store.getState()
+     //);
+   //}, 1000));
   return store;
 }
 
 export default configureStore()
+
+/*/Migrating from redux-saga to redux-toolkit
+//-----Store Setup---//
+const middleware = [thunk]
+
+const initialState = {}
+
+
+
+const store = createStore(reducers, initialState,
+  composeWithDevTools(applyMiddleWare(...middleware)));
+  export default store;*/
